962a57e017219f998dcae574ea4bab648348abfd
Anzhelika Shilyaeva <shilyaeva068@icloud.com>
2023-07-28 16:48:44 +0300

    Добавить ссылку истории в header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import Logo from '../../images/logo.svg';
 import Sort from '../Sort/Sort';
 import './Header.scss';
+import History from '../../images/history.svg';
 
 function Header () {
 return (
   <header className="header">
 
-    <div className='header__hidden-element'></div>
+    <div className='header__history'>
+      <img className='header__history-image' src={History} />
+      <p className='header__text'>История</p>
+    </div>
     <img className='header__logo' src={Logo}/>
     <Sort />
     
